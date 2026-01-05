f331f409fb95171a7f6492b65f1cd410aa821d5f
InMacz <inmacz321@gmail.com>
2026-01-05 01:18:03 -0300

    Update index.js

--- CursoJS/aula10/index.js
+++ CursoJS/aula10/index.js
@@ -6,9 +6,6 @@
 ()
 **
 * / % 
-
-
-
 + -
 Incremento = ++
 Decremento = --
@@ -18,9 +15,3 @@ parseInt (inteiro), parseFloat(decimais)
 const num1 = 10;
 const num2 = Number ('2.5');
 console.log (num1 + num2);
-console.log("gustavo");
-
-
-
-
-
