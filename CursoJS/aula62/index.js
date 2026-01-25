54a1fa4a8e75f0f2480cbabfb1131ff8b6e4f3d5
InMacz <inmacz321@gmail.com>
2026-01-25 18:22:02 -0300

    Update index.js

---
+++ CursoJS/aula62/index.js
@@ -0,0 +1,14 @@
+// REVISÃO ARRAYS
+//                 0          1        2            3         4
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'William', 'Murilo'];
+// nomes.pop(); // pop remove o ultimo elemento do array
+// nomes.shift(); // shift remove o primeiro elemento do array
+// nomes.push('João'); // adiciona um elemento ao final do array
+// nomes.unshift('Joelma') // adiciona um elemento ao começo do array
+// console.log(nomes.lenght); mostra quantos elementos tem o array
+// const novo = nomes.slice(1, 3); // Fatia o array
+// const nomes = nome.split(' '); // Converte uma string em um array
+const nomes = [ 'Luiz', 'Otávio', 'Miranda' ];
+const nome = nomes.join(' ');// Converte um array em uma string
+
+console.log(nome);
