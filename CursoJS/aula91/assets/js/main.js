0de380a29feefdb5500d3d0229147e5a63bfbf83
InMacz <inmacz321@gmail.com>
2026-03-04 16:47:12 -0300

    AULAS

--- CursoJS/aula91/assets/js/main.js
+++ CursoJS/aula91/assets/js/main.js
@@ -1,4 +1,4 @@
-const request = obj => {
+/* const request = obj => {
     return new Promise((resolve, reject) => {
 const xhr = new XMLHttpRequest();
     xhr.open(obj.method, obj.url, true);
@@ -13,7 +13,7 @@ const xhr = new XMLHttpRequest();
     });
     });
     
-};
+}; */
 
 document.addEventListener('click', e =>{
     const el = e.target;
@@ -27,24 +27,29 @@ document.addEventListener('click', e =>{
 
 
 async function carregaPagina(el){
-    const href = el.getAttribute("href");
-
-    const objConfig = {
-        method: 'GET',
-        url: href
-    };
- 
     try{
-        const response = await request(objConfig);
-        carregaResultado(response);
+  const href = el.getAttribute("href");
+    const response = await fetch(href);
+    if(response.status !== 200) throw new Error('ERRO 404!');
+    const html = await response.text();
+    carregaResultado(html)
     }catch(e){
         console.log(e);
     }
-
-
+  
+   
 }
 
 function carregaResultado(response){
 const resultado = document.querySelector('.resultado');
 resultado.innerHTML = response;
 }
+
+
+fetch('pagina4.html')
+    .then(resposta => {
+        if(resposta.status !== 200)throw new Error('ERRO 404 NOSSO');
+        return resposta.text();
+    })
+    .then(html => console.log(html) )
+    .catch(e => console.error(e));
